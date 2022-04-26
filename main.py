8feb42fd4f7d94a9d1479fdaf232b08f4adb6e7a
Md. Towfikul Islam Talukder <towfikislam64@gmail.com>
2022-04-27 00:39:16 +0600

    branch updated

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-import time
-from helpers.scraper import Scraper
-from helpers.csv_helper import get_data_from_csv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from time import sleep
 
+driver_options = Options()
+arguments = [
+    '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+    '--start-maximized',
+    '--disable-blink-features=AutomationControlled',
+]
 
-scraper = Scraper('https://facebook.com')
-scraper.add_login_functionality('https://facebook.com', 'a[href="/me/"]', 'facebook')
-scraper.go_to_page("https://www.facebook.com/groups/371814107751376")
+experimental_options = {
+    'excludeSwitches': ['enable-automation', 'enable-logging'],
+    'prefs': {'profile.default_content_setting_values.notifications': 2}
+}
+
+for argument in arguments:
+    driver_options.add_argument(argument)
+
+for key, value in experimental_options.items():
+    driver_options.add_experimental_option(key, value)
+
+s = Service('C:/chromedriver/chromedriver.exe')
+driver = webdriver.Chrome(service=s, options = driver_options)
+
+browse = driver.get('')
+
+driver.find_element_by_name('').send_keys()
+driver.find_element_by_css_selector("[type=submit]").click()
+sleep(3)
+driver.find_element_by_id("checkbox").click()
