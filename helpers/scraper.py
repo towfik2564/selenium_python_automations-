2abc2705e23424dc5f8f701b88b5f314112c4680
Md. Towfikul Islam <towfikislam64@gmail.com>
2022-05-02 00:52:42 +0600

    selenium argument added

--- helpers/scraper.py
+++ helpers/scraper.py
@@ -40,6 +40,8 @@ class Scraper:
 			'--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
 			'--start-maximized',
 			'--disable-blink-features=AutomationControlled',
+			'--disable-dev-shm-usage',
+			'--no-sandbox'
 		]
 
 		experimental_options = {
